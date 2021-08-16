7316dd93fbd15fb753504f458ef8f5337392d73d
JoaoCasali <82387159+JoaoCasali@users.noreply.github.com>
2021-08-16 19:51:17 -0300

    Tentando criar classes

--- backend/modelo.py
+++ backend/modelo.py
@@ -34,7 +34,7 @@ class Unidade_Saude(db.Model):
 # Classe filho que representa um funcionário da und. de saúde
 class Funcionario(Pessoa):
     CodVerificacao = db.Column(db.String(10)) # Certificação do funcionário
-    UnidadeSaudeId = db.Column(db.Integer, db.ForeignKey(Unidade_Saude.Id), nullable = False)
+    UnidadeSaudeId = db.Column(db.Integer, db.ForeignKey(Unidade_Saude.Id), nullable = True)
     UnidadeSaude = db.relationship('Unidade_Saude') # Associação com a unid. de saúde
 
     def __str__(self):
@@ -54,9 +54,31 @@ class Funcionario(Pessoa):
             "UnidadeSaudeId": self.UnidadeSaudeId,
             "UnidadeSaude": self.UnidadeSaude.json() # Reciclando a função da classe Unidade_Saude
         }
-class Cliente(Pessoa):
+# Classe filho que representa um cliente da und. de saúde       
+class Cidadao(Pessoa):
     Cep = db.Column(db.String(50))
+    Complemento = db.Column(db.String(50))
+    temComorbidades = db.Column(db.Boolean)
+    TipoComorbidades = db.Column(db.String(200))
 
+    def __str__(self):
+        return str(self.Id) + ', ' + self.NomeCompleto + ', ' + self.DtNascimento + ', ' + self.Genero + ', ' + self.Cpf + ', ' + self.Email + ', ' +\
+        self.Senha + ', ' + self.Cep + ', ' + self.Complemento + ', ' + str(self.temComorbidades) + ', ' + self.TipoComorbidades
+    
+    def json(self):
+        return {
+            "id": self.Id,
+            "nome": self.NomeCompleto,
+            "dt_nascimento": self.DtNascimento,
+            "genero": self.Genero,
+            "cpf": self.Cpf,
+            "email": self.Email,
+            "senha": self.Senha,
+            "Cep": self.Cep,
+            "Complemento": self.Complemento,
+            "temComorbidades": self.temComorbidades,
+            "TipoComorbidade": self.TipoComorbidades
+}
 # Bloqueia as seguintes funções quando importado
 if __name__ == "__main__":
     
@@ -71,15 +93,24 @@ if __name__ == "__main__":
     f1 = Funcionario(NomeCompleto = "João Casali", DtNascimento = "11/07/2003", Genero = "M", Cpf = "052.827.732-44", Email = "joaocasali@gmail.com", \
     Senha = "joaozinho123", UnidadeSaude = us1, CodVerificacao = "hbf467hefn")
     
+    c1 = Cidadao(NomeCompleto = "Djenifer Lima", DtNascimento = "20/05/2003", Genero = "F", Cpf = "180.728.569-58", Email = "limadjenifer@gmail.com", \
+    Senha = "joaolindoS2", Cep = "16476261", Complemento = "666", temComorbidades = True, TipoComorbidades = "Cardiopatia|miope|Feia")
     # Adiciona na lista de commit
     db.session.add(us1)
     db.session.add(f1)
+    db.session.add(c1)
     db.session.commit() # Grava os dados no banco de dados
 
-    todas = db.session.query(Funcionario).all() # Traz os dados do banco para uma lista 
-
+    TodosFuncionarios = db.session.query(Funcionario).all() # Traz os dados do banco para uma lista 
+    TodosCidadao = db.session.query(Cidadao).all() # Traz os dados do banco para uma lista
     # Imprime as informações
-    for i in todas:
+    print("")
+    for i in TodosCidadao:
+        print(i)
+        print(i.json())
+        print("")
+    
+    for i in TodosFuncionarios:
         print("")
         print(i)
         print(i.json())
