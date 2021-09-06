0c7415319a90f3b765a5e5c6777253afee128296
JoaoCasali <82387159+JoaoCasali@users.noreply.github.com>
2021-09-06 13:40:09 -0300

    Página de Agendamento
    Fiz umas mudanças minimas em algumas páginas e iniciei a criação de uma página de agendamento

--- script/main.js
+++ script/main.js
@@ -1,8 +1,26 @@
-$( document ).ready(function() {
-    let botao = document.querySelector("#botao");
-	let menu_lateral = document.querySelector(".menu_lateral");
-		  
-	botao.onclick = function() {
-	    menu_lateral.classList.toggle("active");
+$(document).ready(function() {	  
+	$("#botao").click(function(){
+		$(".menu_lateral").toggleClass("active");
+	});
+	var counter = 1;
+	setInterval(function(){
+		$('#radio' + counter)[0].checked = true;
+		counter++;
+		if(counter > 4){
+			counter = 1;
+		}
+	}, 5000);
+
+	$.ajax({
+		url: 'http://localhost:5000/listar_agendamentos',
+		method: 'GET',
+		dataType: 'json', // os dados são recebidos no formato json
+		success: listar_agendamentos, // chama a função listar_agendamentos para processar o resultado
+		error: function() {
+			alert("erro ao ler dados, verifique o backend");
+		}
+	});
+	function listar_agendamentos(resultado){
+		alert(resultado)
 	}
-  });
+});
