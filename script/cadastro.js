bf228eb1cab84bb1b1594b8d72317770c9b0fd04
JoaoCasali <82387159+JoaoCasali@users.noreply.github.com>
2021-09-09 15:41:53 -0300

    Arrumando o cadastro
    Algumas modificações no cadastro

--- script/cadastro.js
+++ script/cadastro.js
@@ -1,15 +1,41 @@
+$(document).ready(function() {
+   $( "#inputCpf" ).keypress(function( event ) {
+      if (event.which > 47 && event.which < 58){
+         mascara_cpf();
+      } else{
+         event.preventDefault();
+      }
+   });
+   $( "#inputCep" ).keypress(function( event ) {
+      if (event.which > 47 && event.which < 58){
+         mascara_cep();
+      } else{
+         event.preventDefault();
+      }
+   });
+});
 // Mascara do CPF
 function mascara_cpf(){
    // Pegando a informação do site
-   var cpf = $('#cpf').val();
+   var cpf = $('#inputCpf').val();
    // Contagem que determina onde terá ponto
    if(cpf.length == 3 || cpf.length == 7){
       // Adicionando ponto no site
-      $('#cpf').val( cpf+'.' );
+      $('#inputCpf').val( cpf+'.' );
    };
    // Contagem que determina onde terá hífen
    if(cpf.length == 11){
       // Adicionando hífen no site
-      $('#cpf').val( cpf+'-' );
+      $('#inputCpf').val( cpf+'-' );
+   };
+};
+// Mascara do CEP
+function mascara_cep(){
+   // Pegando a informação do site
+   var cpf = $('#inputCep').val();
+   // Contagem que determina onde terá hífen
+   if(cpf.length == 5){
+      // Adicionando o hífen no site
+      $('#inputCep').val( cpf+'-' );
    };
 };
