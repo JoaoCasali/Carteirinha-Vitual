523554171e7e2af0575535d8743456934733008f
JoaoCasali <82387159+JoaoCasali@users.noreply.github.com>
2021-09-13 15:01:13 -0300

    Inicio do modal
    Aqui se iniciou a criação de um login

--- script/main.js
+++ script/main.js
@@ -5,10 +5,9 @@ $(document).ready(function() {
 		// Adiciona a classe no menu_lateral caso não tenha, caso tenha, remove dele
 		$(".menu_lateral").toggleClass("active");
 	});
-	$("#log_in").click(function(){
+	$("#logar").click(function(){
 		// Fazendo com que as informações do individuo apareça quando clicar em login
-		$(".profile_content").removeClass("invisivel");
-		$(".login_content").addClass("invisivel");
+		alert("OIi")
 	});
 	$("#log_out").click(function(){
 		// Fazendo com que as informações do individuo desapareça quando clicar em logout
