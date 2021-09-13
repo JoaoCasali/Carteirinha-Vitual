7632a15eaf9eaec3373573f4d07c99cb95f3f926
JoaoCasali <82387159+JoaoCasali@users.noreply.github.com>
2021-09-13 14:09:39 -0300

    Arrumando o cadastro

--- script/cadastro.js
+++ script/cadastro.js
@@ -1,25 +1,41 @@
 $(document).ready(function() {
+   // função que não permite letras no input do CPF
    $("#inputCpf").keypress(function( event ) {
+      // Se for digitado numeros, chama a função de mascara
       if (event.which > 47 && event.which < 58){
          mascara_cpf();
+      // Caso seja digitado qualquer outra coisa, a função acaba
       } else{
          event.preventDefault();
       }
    });
+   // função que não permite letras no input do CEP
    $("#inputCep").keypress(function( event ) {
+      // Se for digitado numeros, chama a função de mascara
       if (event.which > 47 && event.which < 58){
          mascara_cep();
+      // Caso seja digitado qualquer outra coisa, a função acaba
       } else{
          event.preventDefault();
       }
    });
-   $("#cadastrarPessoa").click(function(){
+   $("#verificacao").click(function(){
+      $("#meucadastro").addClass("was-validated");
+   });
+   // Função de envio de cadastro para o banco de dados
+   $("#meucadastro").submit(function(e) {
+      //Cancela a funcionalidade de envio padrão do submit
+      e.preventDefault();
+      // Variavel que define se tem algum erro ou não
+      var semErros = true
+      // Recuperando dados da página
       NomeCompleto = $("#inputNome").val();
       DtNascimento = $("#inputData").val();
       Genero = $('input[name="genero"]:checked').val();
       Cpf = $("#inputCpf").val();
       Cep = $("#inputCep").val();
       Complemento = $("#inputComplemento").val();
+      // Fazendo com que um radio button retorne false ou true
       if ($('input[name="temComorbidades"]:checked').val() == "true"){
          temComorbidades = true
       } else{
@@ -28,42 +44,53 @@ $(document).ready(function() {
       TipoComorbidades = $('#TipoComorbidades').val();
       Email = $("#inputEmail").val();
       Senha = $("#inputSenha").val();
-      var dados = JSON.stringify({ //dados em Json
-         NomeCompleto: NomeCompleto, DtNascimento: DtNascimento, Genero: Genero, Cpf: Cpf, Cep: Cep, Complemento: Complemento, 
-         temComorbidades: temComorbidades, TipoComorbidades: TipoComorbidades, Email: Email, Senha: Senha
-      });
-      $.ajax({
-         url: 'http://localhost:5000/incluir_cidadao',
-         type: 'POST',
-         dataType: 'json', // os dados são recebidos no formato json
-         contentType: 'application/json', // tipo dos dados enviados
-         data: dados, // estes são os dados enviados
-         success: pessoaIncluida, // chama a função listar para processar o resultado
-         error: erroAoIncluir
-     });
-      function pessoaIncluida (retorno) {
-         if (retorno.resultado == "ok") { // a operação deu certo?
-            // informar resultado de sucesso
-            alert("Pessoa incluída com sucesso!");
-            // limpar os campos
-            NomeCompleto = $("#inputNome").val("");
-            DtNascimento = $("#inputData").val("");
-            Genero = $('input[name="genero"]:checked').val("");
-            Cpf = $("#inputCpf").val("");
-            Cep = $("#inputCep").val("");
-            Complemento = $("#inputComplemento").val("");
-            temComorbidades = $('input[name="temComorbidades"]:checked').val("");
-            TipoComorbidades = $('#TipoComorbidades').val("");
-            Email = $("#inputEmail").val("");
-            Senha = $("#inputSenha").val("");
-         } else {
-            // informar mensagem de erro
-            alert(retorno.resultado + ":" + retorno.detalhes);
-            }            
+      // Validação do Email
+      if (Email != $("#inputConfirEmail").val()){
+         semErros = false
+         // Mostrando mensagem de aviso
+         $("#EmailErrado").removeClass("invisivel");
       }
-      function erroAoIncluir (retorno) {
-         // informar mensagem de erro
-         alert("ERRO: "+retorno.resultado + ":" + retorno.detalhes);
+      // Validação da Senha
+      if (Senha != $("#inputConfirSenha").val()){
+         semErros = false
+         // Mostrando mensagem de aviso
+         $("#SenhaErrada").removeClass("invisivel");
+      }
+      // Caso não tenha erros, procede com o commit
+      if(semErros){
+         // transforma os dados em um arquivo json
+         var dados = JSON.stringify({
+            NomeCompleto: NomeCompleto, DtNascimento: DtNascimento, Genero: Genero, Cpf: Cpf, Cep: Cep, Complemento: Complemento, 
+            temComorbidades: temComorbidades, TipoComorbidades: TipoComorbidades, Email: Email, Senha: Senha
+         });
+         // Faz o envio por meio do método ajax
+         $.ajax({
+            url: 'http://localhost:5000/incluir_cidadao', // Endereço do banco de dados
+            type: 'POST', // O tipo POST é o de envio, enquanto GET é o de recuperação de dados
+            dataType: 'json', // Tipo de arquivo que será enviado
+            contentType: 'application/json', // tipo dos dados enviados
+            data: dados, // estes são os dados enviados
+            success: pessoaIncluida, // Mostra uma mensagem indicando o sucesso na operação e limpa o formulário
+            error: erroAoIncluir // Caso de erro, mostra uma mensagem indicando o tal
+         });
+         // Função de sucesso
+         function pessoaIncluida (retorno) {
+            // Se o back-end retornar ok, procede com tais funções
+            if (retorno.resultado == "ok") { 
+               // Alerta que teve sucesso
+               alert("Pessoa incluída com sucesso!");
+               // Redireciona para outra página
+               window.location.href = "index.html";
+            } else {
+               // informar mensagem de erro
+               alert(retorno.resultado + ":" + retorno.detalhes);
+               }            
+         }
+         // Função de erro
+         function erroAoIncluir (retorno) {
+            // informar mensagem de erro
+            alert("ERRO: "+retorno.resultado + ":" + retorno.detalhes);
+         }
       }
    });
 });
