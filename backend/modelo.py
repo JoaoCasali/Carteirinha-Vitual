7686e6012632b12fe27e29e857aa787d34bdab7f
JoaoCasali <82387159+JoaoCasali@users.noreply.github.com>
2021-09-21 14:30:49 -0300

    tabela estoque
    Adição da tabela estoque

--- backend/modelo.py
+++ backend/modelo.py
@@ -26,9 +26,11 @@ class Unidade_Saude(db.Model):
     Complemento = db.Column(db.String(100))
     CodVerificacao = db.Column(db.String(10)) # Será adiocionado um código para a verificação de funcionários
 
+    # Formatação do print no terminal
     def __str__(self):
         return f'{str(self.Id)}, {self.Nome}, {self.Cep}, {self.Complemento}, {self.CodVerificacao}'
     
+    # Criando arquivo Json para envio
     def json(self):
         return {
             "Id": self.Id,
@@ -47,9 +49,11 @@ class Funcionario(Pessoa):
         'polymorphic_identity':'funcionario',
     }
 
+    # Formatação do print no terminal
     def __str__(self):
         return f'{super().__str__()}, {self.CodVerificacao}, {str(self.UnidadeSaudeId)}, {str(self.UnidadeSaude)}, {self.Type}' 
 
+    # Criando arquivo Json para envio
     def json(self):
         return {
             "Id": self.Id,
@@ -74,9 +78,11 @@ class Cidadao(Pessoa):
         'polymorphic_identity':'cidadao',
     }
 
+    # Formatação do print no terminal
     def __str__(self):
         return f'{super().__str__()}, {self.Cep}, {self.Complemento}, {str(self.temComorbidades)}, {self.TipoComorbidades}, {self.Type}'
     
+    # Criando arquivo Json para envio
     def json(self):
         return {
             "Id": self.Id,
@@ -92,18 +98,24 @@ class Cidadao(Pessoa):
             "TipoComorbidade": self.TipoComorbidades,
             "Type": self.Type
             }
+
+# Casse que representa os agendamentos de vacina
 class Agendamento(db.Model):
     Id = db.Column(db.Integer, primary_key=True)
     Vacina = db.Column(db.String(200))
     DtAgendamento = db.Column(db.String(200))
+    # Conexão com o cidadão
     IdCidadao = db.Column(db.Integer, db.ForeignKey(Pessoa.Id), nullable = True)
-    UnidadeSaudeId = db.Column(db.Integer, db.ForeignKey(Unidade_Saude.Id), nullable = True)
     Cidadao = db.relationship('Pessoa')
+    # Conexão com a unidade de saude
+    UnidadeSaudeId = db.Column(db.Integer, db.ForeignKey(Unidade_Saude.Id), nullable = True)
     UnidadeSaude = db.relationship('Unidade_Saude')
 
+    # Formatação do print no terminal
     def __str__(self):
-        return f'{str(self.Id)}, {self.Vacina}, {self.DtAgendamento}, {str(self.IdCidadao)}, {str(self.UnidadeSaudeId)}, {self.Cidadao}, s{self.UnidadeSaude}'
+        return f'{str(self.Id)}, {self.Vacina}, {self.DtAgendamento}, {str(self.IdCidadao)}, {str(self.UnidadeSaudeId)}, {self.Cidadao}, {self.UnidadeSaude}'
     
+    # Criando arquivo Json para envio
     def json(self):
         return {
             "Id": self.Id,
@@ -112,7 +124,30 @@ class Agendamento(db.Model):
             "IdCidadao": self.IdCidadao,
             "UnidadeSaudeId": self.UnidadeSaudeId,
             "Cidadao": self.Cidadao.json(),
-            "UnidadeSaude": self.UnidadeSaude.json(),
+            "UnidadeSaude": self.UnidadeSaude.json()
+            }
+
+# Classe que representa o estoque da Unidade de Saude
+class Estoque(db.Model):
+    Id = db.Column(db.Integer, primary_key=True)
+    QtdVacina = db.Column(db.Integer)
+    Descricao = db.Column(db.String(200))
+    # Conexão com a unidade de saude
+    UnidadeSaudeId = db.Column(db.Integer, db.ForeignKey(Unidade_Saude.Id), nullable = True)
+    UnidadeSaude = db.relationship('Unidade_Saude')
+
+    # Formatação do print no terminal
+    def __str__(self):
+        return f'{str(self.Id)}, {self.QtdVacina}, {self.Descricao}, {str(self.UnidadeSaudeId)}, {self.UnidadeSaude}'
+    
+    # Criando arquivo Json para envio
+    def json(self):
+        return {
+            "Id": self.Id,
+            "QtdVacina": self.QtdVacina,
+            "Descricao": self.Descricao,
+            "UnidadeSaudeId": self.UnidadeSaudeId,
+            "UnidadeSaude": self.UnidadeSaude.json()
             }
 
 # Bloqueia as seguintes funções quando importado
@@ -132,11 +167,13 @@ if __name__ == "__main__":
     c1 = Cidadao(NomeCompleto = "Djenifer Lima", DtNascimento = "2003-05-20", Genero = "F", Cpf = "180.728.569-58", Email = "limadjenifer@gmail.com", \
     Senha = "joaolindoS2", Cep = "16476261", Complemento = "666", temComorbidades = True, TipoComorbidades = "Cardiopatia|miope|Feia")
     a1 = Agendamento(Vacina = "Covid-19", DtAgendamento = "2021-09-27", Cidadao = c1, UnidadeSaude = us1)
+    e1 = Estoque(QtdVacina = "300", Descricao = "Covid-19 pfizer", UnidadeSaude = us1)
     # Adiciona na lista de commit
     db.session.add(us1)
     db.session.add(f1)
     db.session.add(c1)
     db.session.add(a1)
+    db.session.add(e1)
     db.session.commit() # Grava os dados no banco de dados
 
     TodosPessoa = db.session.query(Pessoa).all() # Traz os dados do banco para uma lista 
@@ -149,6 +186,12 @@ if __name__ == "__main__":
 
     TodosAgendamento = db.session.query(Agendamento).all()
     for i in TodosAgendamento:
+        print(i)
+        print(i.json())
+        print("")
+    
+    TodosEstoque = db.session.query(Estoque).all()
+    for i in TodosEstoque:
         print(i)
         print(i.json())
         print("")
