2ef069c0a2fcf11fd8a92cd67d44fd5362931081
JoaoCasali <82387159+JoaoCasali@users.noreply.github.com>
2021-10-18 17:08:45 -0300

    Deixando a página do usuário mutavel

--- script/usuario.js
+++ script/usuario.js
@@ -1,17 +1,34 @@
 $(document).ready(function() {
-    $('#numerodapessoa').val(sessionStorage.Id);
-    $('#upload-file-btn').click(function() {
-        var form_data = new FormData($('#upload-file')[0], sessionStorage.Id);
-        $.ajax({
-            type: 'POST',
-            url: 'http://localhost:5000/upload_file',
-            data: form_data,
-            contentType: false,
-            cache: false,
-            processData: false,
-            success: function(data) {
-                console.log('Success!');
-            },
-        });
-    });
+    $('#NomeUsuario').append(sessionStorage.NomeCompleto);
+    $('#CPFUsuario').append(sessionStorage.Cpf);
+    $('#DtNascimentoUsuario').append(sessionStorage.DtNascimento);
+    if (sessionStorage.Genero == "F"){
+        $('#GeneroUsuario').append("Feminino");
+    } else {
+        $('#GeneroUsuario').append("Masculino");
+    }
+    if (sessionStorage.temComorbidades == "true"){
+        $('#TemComorbidade').append("Possui");
+    } else {
+        $('#TemComorbidade').append("Não Possui");
+    }
+    $('#EmailUsuario').append(sessionStorage.Email);
+    $('#CEPUsuario').append(sessionStorage.Cep);
+
+
+    // $('#numerodapessoa').val(sessionStorage.Id);
+    // $('#upload-file-btn').click(function() {
+    //     var form_data = new FormData($('#upload-file')[0], sessionStorage.Id);
+    //     $.ajax({
+    //         type: 'POST',
+    //         url: 'http://localhost:5000/upload_file',
+    //         data: form_data,
+    //         contentType: false,
+    //         cache: false,
+    //         processData: false,
+    //         success: function(data) {
+    //             console.log('Success!');
+    //         },
+    //     });
+    // });
 });
